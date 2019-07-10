6c3e7a165d2b854fb3041cda01ae733fe2a9aa03
Justin Alberto Castillo Valladares <justincastillovalladares@gmail.com>
2019-07-10 13:18:19 -0600

    feat: function to save service in service-controller

--- server/controllers/service-controller.js
+++ server/controllers/service-controller.js
@@ -1,4 +1,4 @@
-const db = require('../config/db');
+const db = require("../config/db");
 
 const pg = require("pg");
 var client;
@@ -12,7 +12,18 @@ function saveService(req, res) {
       res.status(400).send(err);
       console.log(`err when connecting on getProducts: ${err}`);
     } else {
-      let query = `SELECT * FROM get_products()`;
+      let service = req.body;
+      let query = {
+        text: "SELECT * FROM save_service($1, $2, $3, $4, $5, $6)",
+        values: [
+          service.location,
+          service.name,
+          service.classification,
+          service.additional_info,
+          service.phone,
+          service.email
+        ]
+      };
       client
         .query(query)
         .then(data => {
---
+++ sql/scripts.sql
@@ -0,0 +1,23 @@
+CREATE TABLE service(
+  id SERIAL PRIMARY KEY,
+  location VARCHAR NOT NULL,
+  name VARCHAR NOT NULL,
+  classification VARCHAR NOT NULL,
+  additional_info VARCHAR NOT NULL,
+  phone VARCHAR NOT NULL,
+  email VARCHAR NOT NULL
+);
+
+CREATE TABLE images(
+  id SERIAL PRIMARY KEY,
+  url VARCHAR NOT NULL,
+  id_service_fk INTEGER REFERENCES service(id)
+);
+
+CREATE OR REPLACE FUNCTION save_service(location VARCHAR, name VARCHAR, classification VARCHAR, additional_info VARCHAR, phone VARCHAR, email VARCHAR)
+  RETURNS VOID AS
+  $$
+    BEGIN
+      INSERT INTO service VALUES (location, name, classification, additional_info, phone, email);
+    END;
+  $$ LANGUAGE plpgsql;
