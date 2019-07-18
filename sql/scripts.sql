179dbbd0dd0bc6df251fae9e0ec4a630d8445833
Justin Alberto Castillo Valladares <justincastillovalladares@gmail.com>
2019-07-18 10:35:16 -0600

    feat: last changes

--- sql/scripts.sql
+++ sql/scripts.sql
@@ -30,3 +30,31 @@ CREATE OR REPLACE FUNCTION save_image(url VARCHAR, id_service INTEGER)
       INSERT INTO images VALUES(url, id_service);
     END;
   $$ LANGUAGE plpgsql;
+
+CREATE OR REPLACE FUNCTION get_services()
+RETURNS TABLE(
+    _name VARCHAR,
+    _location VARCHAR,
+    _classification VARCHAR,
+    _additional_info VARCHAR,
+    _phone VARCHAR,
+    _email VARCHAR,
+    id_service INTEGER,
+    imgs VARCHAR[]
+ )
+ AS
+    $$
+        BEGIN
+            RETURN QUERY SELECT
+                   s.name _name,
+                   s.location _location,
+                   s.classification _classification,
+                   s.additional_info _additional_info,
+                   s.phone _phone,
+                   s.email _email,
+                   s.id id_service,
+                   array_agg(i.url) as imgs
+            FROM service AS s INNER
+            JOIN images i on s.id = i.id_service_fk group by s.id;
+        END;
+    $$ LANGUAGE plpgsql;
