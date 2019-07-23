fee12525c9c57c017c8e43205342f83b9087fb8b
Justin Alberto Castillo Valladares <justincastillovalladares@gmail.com>
2019-07-22 20:49:25 -0600

    feat: logout btn in navbar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { DialogManagerService } from './services/dialog-manager.service';
+import { AuthService } from './services/auth.service';
 
 @Component({
   selector: 'app-root',
@@ -10,13 +11,14 @@ export class AppComponent {
   title = 'ruta-turistica-del-norte';
   date: Date = new Date();
   navbarOpen = false;
+  localStorage = localStorage;
 
   toggleNavbar() {
     this.navbarOpen = !this.navbarOpen;
   }
 
 
-  constructor(private _dialog: DialogManagerService) {}
+  constructor(private _dialog: DialogManagerService, private _auth: AuthService) {}
 
   openLoginDialog() {
     this._dialog.openLoginDialog();
