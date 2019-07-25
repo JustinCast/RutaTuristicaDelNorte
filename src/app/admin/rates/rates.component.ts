79fed79330547a9250177f77b9c5656c30ba7def
Justin Alberto Castillo Valladares <justincastillovalladares@gmail.com>
2019-07-24 20:41:28 -0600

    feat: working on rates form

--- src/app/admin/rates/rates.component.ts
+++ src/app/admin/rates/rates.component.ts
@@ -18,9 +18,7 @@ export class RatesComponent implements OnInit {
   ngOnInit() {
     this.ratesFG = this._fb.group({
       header1: ["", Validators.required],
-      header2: ["", Validators.required],
-      value1: ["", Validators.required],
-      value2: ["", Validators.required],
+      header2: ["Tarifa Neta", Validators.required],
       items: this._fb.array([this.createItem()])
     });
   }
@@ -31,8 +29,8 @@ export class RatesComponent implements OnInit {
 
   createItem(): FormGroup {
     return this._fb.group({
-      v1: "",
-      v2: ""
+      v1: ["", Validators.required],
+      v2: ["", Validators.required]
     });
   }
 
@@ -41,22 +39,11 @@ export class RatesComponent implements OnInit {
     this.items.push(this.createItem());
   }
 
-  addRow() {
-    // Find a <table> element with id="myTable":
-    let cellCode = `<mat-form-field>
-                      <input matInput type="text" placeholder="Inserte un valor" />
-                    </mat-form-field>`;
-    var table = document.getElementById("rates");
+  deleteItem(index) {
+    this.items.removeAt(index);
+  }
 
-    // Create an empty <tr> element and add it to the 1st position of the table:
-    var row = table.insertRow(-1);
-
-    // Insert new cells (<td> elements) at the 1st and 2nd position of the "new" <tr> element:
-    var cell1 = row.insertCell(0);
-    var cell2 = row.insertCell(1);
-
-    // Add some text to the new cells:
-    cell1.innerHTML = cellCode;
-    cell2.innerHTML = cellCode;
+  onSubmit() {
+    console.log(this.ratesFG.value);
   }
 }
