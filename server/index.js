089f71f418d07452735de3629e9de03d948a5f6e
JustinCast <justincastillovalladares@gmail.com>
2019-10-19 17:31:48 -0600

    feat: angular dir updated

--- server/index.js
+++ server/index.js
@@ -34,7 +34,7 @@ function routerConfig() {
   app.use(express.static("../dist/ruta-turistica-del-norte"));
   app.use('/api', api);
   app.get("/*", function(req, res) {
-    res.sendFile(path.join("../dist/ruta-turistica-del-norte/index.html"));
+    res.sendFile(path.join(__dirname + "../dist/ruta-turistica-del-norte/index.html"));
   });
 }
 
